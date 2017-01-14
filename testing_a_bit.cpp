c506b88fc625cf75e97cf7b62318a6ec52dbb15d
mxd <mxd789@gmail.com>
2017-01-14 18:29:24 -0500

    third commit

---
+++ testing_a_bit.cpp
@@ -0,0 +1,28 @@
+// testing_a_bit.cpp
+#include <iostream>
+using namespace std;
+#define MASK0 0x01
+#define MASK1 0x02
+#define MASK2 0x04
+#define MASK3 0x08
+int main()
+{
+	char flags=0x05;
+	if((flags&MASK3)==MASK3)
+		cout<<"Letter A\n";
+	else
+		cout<<"No Letter A\n";
+	if((flags&MASK2)==MASK2)
+		cout<<"Letter B\n";
+	else
+		cout<<"No Letter B\n";
+	if((flags&MASK1)==MASK1)
+		cout<<"Letter C\n";
+	else
+		cout<<"No Letter C\n";
+	if((flags&MASK0)==MASK0)
+		cout<<"Letter D\n";
+	else
+		cout<<"No Letter D\n";
+	return 0;
+}
