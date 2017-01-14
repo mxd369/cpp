30434353de289909e7a06ea5dee7b70cffcf4bfb
mxd <mxd789@gmail.com>
2017-01-14 18:35:29 -0500

    fourth commit

---
+++ bitwise_operators.cpp
@@ -0,0 +1,12 @@
+// bitwise_operators
+#include <iostream>
+using namespace std;
+int main(){
+	cout << (5&3) << endl;
+	cout << (5|3) << endl;
+	cout << (5^3) << endl;
+	cout << ~5 << endl;
+	cout << (6<<3) << endl;
+	cout << (6>>1) << endl;
+	return 0;
+}
