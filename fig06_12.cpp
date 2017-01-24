4a872284b66157b0f8fc7e2285d65d9c8fcda918
mxd <mxd789@gmail.com>
2017-01-23 23:03:26 -0500

    functions, activation record, function-call stack

---
+++ fig06_12.cpp
@@ -0,0 +1,18 @@
+// Fig. 6.12: fig06_12.cpp
+// square function used to demonstrate the function
+// call stack and activation records/
+#include <iostream>
+using namespace std;
+
+int square(int);// prototype for function square
+
+int main() {
+	int a{10}; // value to square (local variable in main)
+
+	cout << a << " squared: " << square(a) << endl; // display a squared 
+}
+
+// returns teh square of an integer
+int square(int x) { // x is a local variable
+	return x * x;	// calculate a square and return result
+}
