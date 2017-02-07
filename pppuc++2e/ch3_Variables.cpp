a5d55090be5d4ac25c0fab26c50fdf674616b8d2
mxd <mxd789@gmail.com>
2017-02-07 12:00:17 -0500

    continuing exercises

---
+++ pppuc++2e/ch3_Variables.cpp
@@ -0,0 +1,21 @@
+// 3.2 Variables
+#include "std_lib_facilities.h"
+using namespace std;
+int main()
+{
+	string name = "Annemarie";
+	// int for integers
+	int number_of_steps = 39;
+	// The compiler remembers the type of each variable and makes sure that you use it according to its type, as specified in its definition.
+
+	// 5 basic types
+	// double for floating-point numbers
+	double flying_time = 3.5;
+	char decimal_point = '.'; // char for individual characters
+	string name = "Annemarie";// string for character strings
+	bool tap_on = true;		  // bool for logical variables
+
+
+
+	return 0;
+}
