0bb9f5623756c5c59ff85b21be1c425e390c5066
mxd <mxd789@gmail.com>
2017-02-11 23:16:03 -0500

    continuing

---
+++ lab/lab03_test/exclusive_tester/exclusive_tester/main.cpp
@@ -0,0 +1,55 @@
+//
+//  main.cpp
+//  exclusive_tester
+//
+//  Created by MXD on 2/11/17.
+//  Copyright © 2017 MXD. All rights reserved.
+//
+#include <iostream>
+using namespace std;
+
+int main(){
+/*
+    int counter;
+    string stream1;
+    string storage1;
+    string x;
+    stringstream stream1(inputfromuser);
+
+    while(stream1 >> storage1)
+    {
+
+        stringstream stream2(inputfromuser);
+        while(stream2 >> storage2)
+        {
+            if(storage1 == storage2){
+                //have a counter here or something to ++
+                counter++;
+            }
+    }
+
+    if(counter == 1)
+    {
+        cout << storage1 << endl;
+    }
+    else {
+        counter --;
+    }
+        
+    return 0;
+}
+ */
+    while(stream1 >> storage1){
+ 
+        stringstream stream2(inputfromuser);
+ 
+        while(stream2 >> storage2){
+            if(storage1 == storage2)
+                //have a counter here or something to ++
+        }
+ 
+        if(check if the counter is 1)
+            //this is the exclusive number
+        else
+            //reset counter
+ }
--- lab/lab03_test/raunak_dey_exclusive.cpp
+++ lab/lab03_test/raunak_dey_exclusive.cpp
@@ -2,24 +2,22 @@
 #include <sstream>
 using namespace std;
 int main(){
-
+/*
 	getline(cin, ourinput);
-	//use the above to get the input
-
- 
+	// use the above to get the input
 	stringstream hello(ourinput);// for the outer loop 
 	stringstream hi(ourinput);//for the nested loop
-	//take the input in string stream
+	// take the input in string stream
 		while(hello >> storage){
-			//basically here you are putting one number at a time into storage variable. If you print storage u will get a single value
-			//do more stuff here 
+			// basically here you are putting one number at a time into storage variable.
+			// If you print storage u will get a single value
+			// do more stuff here 
 	}
 	return 0;
 }
-
+*/
 
 /*
-
 while(stream1 >> storage1){
 
     stringstream stream2(inputfromuser);
@@ -33,5 +31,26 @@ while(stream1 >> storage1){
     else
       reset counter
   }
-
 */
+
+int counter;
+string stream1;
+string storage1;
+
+while(stream1 >> storage1){
+
+    stringstream stream2(inputfromuser);
+    while(stream2 >> storage2){
+      if(storage1 == storage2)
+        //have a counter here or something to ++
+          counter++;
+    }
+
+    if(counter == 1)
+      cout << storage1 << endl;
+    else
+      counter --;
+  }
+
+  return 0;
+}
