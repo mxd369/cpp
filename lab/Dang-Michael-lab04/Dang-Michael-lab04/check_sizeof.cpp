a460d237f9ce4f81146456293417cddc0e820206
mxd <mxd789@gmail.com>
2017-02-15 00:01:43 -0500

    lab04

---
+++ lab/Dang-Michael-lab04/Dang-Michael-lab04/check_sizeof.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+int main(){
+
+  char a;
+  //char & a;
+  cout << "(a) char " << sizeof a << '\n';
+  cout << "(b) char & " << sizeof & a << '\n';
+  //cout << "(c) char * " << sizeof x << '\n';
+  //cout << "(d) short " << sizeof x << '\n';
+  //cout << "(e) short & " << sizeof x << '\n';
+  //cout << "(f) short * " << sizeof x << '\n';
+  //cout << "(g) int " << sizeof x << '\n';
+  //cout << "(h) int & " << sizeof x << '\n';
+  //cout << "(i) int * " << sizeof x << '\n';
+  //cout << "(j) long " << sizeof x << '\n';
+  //cout << "(k) long & " << sizeof x << '\n';
+  //cout << "(l) long * " << sizeof x << '\n';
+  //cout << "(m) int ** " << sizeof x << '\n';
+  //cout << "(n) int *** " << sizeof x << '\n';
+
+  return 0;
+}
+/*
+(a) char
+(b) char &
+(c) char *
+(d) short
+(e) short &
+(f) short *
+(g) int
+(h) int &
+(i) int *
+(j) long
+(k) long &
+(l) long *
+(m) int **
+(n) int ***
+*/
