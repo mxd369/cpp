b0d107f8c00b5fdc1e474ba7d155eac0095c357a
mxd <mxd789@gmail.com>
2017-02-22 21:26:56 -0500

    continuing

--- project/p1.cpp
+++
@@ -1,7 +0,0 @@
-/*
-p1.cpp:
-This file should contain a small/moderately sized driver that demonstrates the full range of functionality of your Matrix class.
-
-Additionally, make sure that you adhere to the following:
-All functions must be documented using Javadoc-style comments. Use inline documentation, as needed, to explain ambiguous or tricky parts of your code.
-*/
